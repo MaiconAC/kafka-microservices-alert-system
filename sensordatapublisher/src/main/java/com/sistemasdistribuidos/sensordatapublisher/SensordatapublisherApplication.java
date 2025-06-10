5574db60d4515dcb57f4ae68008d02a92a37a932
Maicon Antonio Cunico <maiconcunico@gmail.com>
2025-06-10 20:16:01 -0300

    chore: criado projeto inicial do sensor-publisher

---
+++ sensordatapublisher/src/main/java/com/sistemasdistribuidos/sensordatapublisher/SensordatapublisherApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasdistribuidos.sensordatapublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensordatapublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensordatapublisherApplication.class, args);
+	}
+
+}
---
+++ sensordatapublisher/src/test/java/com/sistemasdistribuidos/sensordatapublisher/SensordatapublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasdistribuidos.sensordatapublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensordatapublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
